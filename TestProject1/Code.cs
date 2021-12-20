1af34ed97ed1d797e0ab350c23bbc2f546ed992b
MSI <MSI@DESKTOP-LLU1IS4>
2021-12-20 19:57:05 +0300

    Добавьте файлы проекта.

---
+++ TestProject1/Code.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLabWorkNomer13
+{
+    public class Code
+    {
+        static void Main(string[] args)
+        {
+            Tannel auto = new Tannel("Туннель");
+            Vxod auto2 = new Vxod("Вход");
+            Svet auto3 = new Svet("свечение");
+            Speed auto4 = new Speed("Скорость");
+
+            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7}", auto.name, auto.Deistvie(), auto2.name, auto2.Deistvie2(), auto3.name, auto3.Deistvie3(), auto4.name, auto4.Diestvie4());
+            Console.ReadKey();
+        }
+    }
+
+    public class Tannel
+    {
+        public string name { get; set; }
+        public Tannel(string name)
+        {
+            this.name = name;
+        }
+
+        public string Deistvie()
+        {
+            return "Прошло несколько секунд,прежде чем он понял,что произошло. Они нырнули в туннель.";
+        }
+    }
+
+    public class Vxod
+    {
+        public string name { get; set; }
+        public Vxod(string name)
+        {
+            this.name = name;
+        }
+
+        public string Deistvie2()
+        {
+            return "То,что он принял за движущийся им навстречу объект,было его освещённым входом";
+        }
+    }
+
+    public class Svet
+    {
+        public string name { get; set; }
+        public Svet(string name)
+        {
+            this.name = name;
+        }
+
+        public string Deistvie3()
+        {
+            return "Серебристое свечение испускали круглые стены туннеля,";
+        }
+    }
+
+    public class Speed
+    {
+        public string name { get; set; }
+        public Speed(string name)
+        {
+            this.name = name;
+        }
+
+        public string Diestvie4()
+        {
+            return "очевидно,со скоростью несколько сот миль в час.";
+        }
+    }
+}
---
+++ UnitTestProject1/TestPLW13.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestProjectLabWorkNomer13;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestPLW13
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Tannel auto = new Tannel("Туннель");
+            Vxod auto2 = new Vxod("Вход");
+
+            Assert.AreEqual("Туннель", auto.name);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Tannel auto = new Tannel("Туннель");
+            Assert.AreEqual("Прошло несколько секунд,прежде чем он понял,что произошло. Они нырнули в туннель.", auto.Deistvie());
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Vxod auto2 = new Vxod("Вход");
+            Svet auto3 = new Svet("Свет");
+
+            Assert.AreEqual("Вход", auto2.name);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Vxod auto2 = new Vxod("Вход");
+            Assert.AreEqual("То,что он принял за движущийся им навстречу объект,было его освещённым входом", auto2.Deistvie2());
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Svet auto3 = new Svet("свечение");
+            Speed auto4 = new Speed("Скорость");
+
+            Assert.AreEqual("свечение", auto3.name);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Svet auto3 = new Svet("Свет");
+            Assert.AreEqual("Серебристое свечение испускали круглые стены туннеля,", auto3.Deistvie3());
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Speed auto4 = new Speed("Скорость");
+            Assert.AreEqual(auto4.name,"Скорость");
+        }
+
+        [TestMethod]
+        public void TestMothod8()
+        {
+            Speed auto4 = new Speed("Скорость");
+            Assert.AreEqual(auto4.Diestvie4(),"очевидно,со скоростью несколько сот миль в час.");
+        }
+    }
+}
